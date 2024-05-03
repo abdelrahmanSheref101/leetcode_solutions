5a0f88a2b8734832f8f6d249d4a01beaa52fad7e
abdelrahmanSheref101 <160889436+abdelrahmanSheref101@users.noreply.github.com>
2024-05-03 20:45:23 +0300

    Time: 0 ms (100.00%), Space: 7.7 MB (31.29%) - LeetHub

---
+++ 3114-latest-time-you-can-obtain-after-replacing-characters/3114-latest-time-you-can-obtain-after-replacing-characters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string findLatestTime(string s) {
+        string res=s;
+        if(res[0]=='?'){
+            if(res[1]=='1'||res[1]=='0'||res[1]=='?')
+                res[0]='1';
+            else
+                res[0]='0';
+        }
+        if(res[1]=='?'){
+            if(res[0]=='1')
+               res[1]='1'; 
+            else
+                res[1]='9';
+        }
+        if(res[3]=='?'){
+            res[3]='5';
+        }
+        if(res[4]=='?'){
+            res[4]='9';
+        }
+    return res;
+    }
+};
